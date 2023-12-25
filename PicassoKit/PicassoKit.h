294b5a3a9982a45da2622e7dce448a4193ece1c5
Aviel Gross <avielgross@gmail.com>
2023-12-25 11:47:58 -0500

    more renames

--- PicassoKit/PicassoKit.h
+++ PicassoKit/PicassoKit.h
@@ -1,18 +1,18 @@
 //
-//  PicassoFramework.h
-//  PicassoFramework
+//  PicassoKit.h
+//  PicassoKit
 //
 //  Created by Aviel Gross on 24.12.2023.
 //
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for PicassoFramework.
+//! Project version number for PicassoKit.
 FOUNDATION_EXPORT double PicassoKitVersionNumber;
 
-//! Project version string for PicassoFramework.
+//! Project version string for PicassoKit.
 FOUNDATION_EXPORT const unsigned char PicassoKitVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <PicassoFramework/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <PicassoKit/PublicHeader.h>
 
 
